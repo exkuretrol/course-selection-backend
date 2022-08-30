63f56ac2cc4f5ee565a6e8b8bce8d9d6c4c0434d
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2022-08-30 21:40:42 +0800

    add submodule and update ner json format

--- python/pyserver.py
+++ python/pyserver.py
@@ -3,12 +3,17 @@ from flask_cors import CORS, cross_origin
 from pypinyin import Style, pinyin
 from func.helper import findTeacherSubject
 import json
+from ckip_transformers.ckip_transformers.nlp import CkipWordSegmenter, CkipPosTagger, CkipNerChunker
+
+print("Initializing drivers ... NER")
+ner_driver = CkipNerChunker(model_name="./test-ner3600")
+print("Initializing drivers ... done")
 
 app = Flask(__name__)
 CORS(app, resources={
     r"/.*": {
         "origins": [
-            "http://127.0.0.1:3000", 
+            "http://127.0.0.1:3000",
             "http://localhost:3000"
         ]
     }
@@ -30,6 +35,28 @@ def filter():
     text = req['text']
     sub, thr = findTeacherSubject(text)
     return json.dumps({"subject": sub, "teacher": thr})
+
+
+@app.route('/api/ner', methods=['POST'])
+def ner():
+    req = request.get_json()
+    if (req['multiple']):
+        text = req['text']
+    else:
+        text = [req['text']]
+
+    ner = ner_driver(text)
+
+    def format2json(ner_item):
+        return [
+            {
+                "word": entity.word,
+                "tag": entity.ner,
+                "idx": [entity.idx[0], entity.idx[1]]
+            } for entity in ner_item
+        ]
+    
+    return json.dumps({"result": [format2json(nerr) for nerr in ner]});
 
 if __name__ == "__main__":
     app.run(port=5000)
