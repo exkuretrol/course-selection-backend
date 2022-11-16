cedeaa513bd38f939b24bca84be369e3d83b246c
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2022-11-16 19:23:37 +0800

    add mysql package

--- python/func/ner_query.py
+++ python/func/ner_query.py
@@ -1,44 +1,13 @@
+import string
 from typing import List
 from pypinyin import pinyin, Style
 import re
 
-subject = []       #課程
-time_lesson = []   #時間_節
-time_week = []     #時間_週
-people = []        #老師
-department = []    #系別
-category = []      #選別
-grade = []         #年級
-campus = []        #校區
-curriculum = []    #課表
-credit = []        #學分
-semester = []      #學期
-education = []     #制別
-internship = []    #實習課
-graduate = []      #畢業班
-# location = []      #地點
-Class = []         #班級
-lesson = {'零':0,'一':1, '二':2, '三':3, '四':4, '五':5, '六':6, '七':7, '八':8, '九':9}
-Department = {'管理學院':'1152545657','國際企業學系':'57','國企系':'57','國企':'57','風險管理與保險學系':'56','風保系':'56','風保':'56',
-                '會計學系':'52','會計系':'52','會計':'52','企業管理學系':'11','企管系':'11','企管':'11','財務金融學系':'54','財金系':'54',
-                '財金':'54','資訊學院':'0513163637','資訊管理學系':'13','資管系':'13','資管':'13','資訊傳播工程學系':'16','資傳系':'16',
-                '資傳':'16','資訊工程學系':'36','資工系':'36','資工':'36','電腦與通訊工程學系':'05','電通系':'05','電通':'05',
-                '電子工程學系':'37','電子系':'37','電工系':'37','電子':'37','電工':'37','傳播學院':'26313233','廣播電視學系':'32','廣電系':'32',
-                '廣電':'32','新聞學系':'33','新聞系':'33','新聞':'33','廣告暨策略行銷學系':'26','廣銷系':'26','廣銷':'26','新媒體暨傳播管理學系':'31',
-                '新傳系':'31','新傳':'31','法律學院':'4165','法律學系':'41','法律系':'41','法律':'41','財金法律系':'65','財法系':'65','財法':'65',
-                '觀光學院':'141819','觀光事業學系':'14','觀光系':'14','觀光':'14','休閒遊憩管理學系':'18','休憩系':'18','休憩':'18',
-                '餐旅管理學系':'19','餐旅系':'19','餐旅':'19','教育暨應用語文學院':'4243444583','應用中國文學系':'43','應中系':'43','應中':'43',
-                '應用英語學系':'42','應英系':'42','應英':'42','應用日語學系':'44','應日系':'44','應日':'44','教育研究所':'83','華語文教學學系':'45',
-                '華教系':'45','華教':'45','設計學院':'040921232450','商業設計學系':'21','商設系':'21','商設':'21','商品設計學系':'23','品設系':'23',
-                '品設':'23','數位媒體設計學系':'09','數媒系':'09','數媒':'09','都市規劃與防災學系':'04','都防系':'04','都防':'04','建築學系':'24',
-                '建築系':'24','建築':'24','動漫文創設計學士學位學程':'50','動漫學程':'50','社會科學院':'083868','公共事務與行政管理學系':'68',
-                '公事系':'68','公事':'68','諮商與工商心理學系':'08','心理系':'08','心理':'08','犯罪防治學系':'38','犯防系':'38','犯防':'38',
-                '健康科技學院':'394067','生物科技學系':'39','生科系':'39','生科':'39','醫療資訊與管理學系':'40','醫管系':'40','醫管':'40',
-                '生物醫學工程學系':'67','醫工系':'67','醫工':'67','國際學院':'252747485991','國際事務與外交學士學位學程':'59',
-                '國際企業與貿易學士學位學程':'91','新聞與大眾傳播學士學位學程':'27','時尚與創新管理學士學位學程':'25','旅遊與觀光學士學位學程':'48',
-                '資訊科技應用與管理學士學位學程':'47','金融科技學院':'172246','經濟與金融學系':'22','經濟系':'22','經濟':'22','應用統計與資料科學學系':'17',
-                '統資系':'17','統資':'17','金融科技應用學士':'46','金科系':'46','金科':'46'
-            }
+def toZhuYin(word: string):
+    se = pinyin(word, style=Style.BOPOMOFO)  #注音
+    se = "".join([item for innerlist in se for item in innerlist])  #刪[]
+    se = re.sub('[ˊˇˋ˙]','',se)
+    return se
 
 class NerToken:
   def __init__(self, word, ner, idx):
@@ -49,8 +18,26 @@ class NerToken:
     return "NerToken(word='" + self.word + "', ner='" + self.ner + "', idx=[" + str(self.idx[0]) + ", " + str(self.idx[1]) + "]" + ")"
 
 def ner_query(text: List[NerToken]):
-    # people_count = 0 #老師數量
-    # subject_count = 0 #課程數量
+    subject = []       #課程
+    time_lesson = []   #時間_節
+    time_week = []     #時間_週
+    people = []        #老師
+    department = []    #系別
+    category = []      #選別
+    grade = []         #年級
+    campus = []        #校區
+    # 老師課表不需要的別獨立出一個類別
+    # curriculum = []    #課表
+    credit = []        #學分
+    semester = []      #學期
+    education = []     #制別
+    internship = []    #實習課
+    graduate = []      #畢業班
+    # location = []      #地點
+    Class = []         #班級
+    lesson = {'零':0,'一':1, '二':2, '三':3, '四':4, '五':5, '六':6, '七':7, '八':8, '九':9}
+    Department = {'統之系': 17, '管理學院':'1152545657','國際企業學系':'57','國企系':'57','國企':'57','風險管理與保險學系':'56','風保系':'56','風保':'56', '會計學系':'52','會計系':'52','會計':'52','企業管理學系':'11','企管系':'11','企管':'11','財務金融學系':'54','財金系':'54', '財金':'54','資訊學院':'0513163637','資訊管理學系':'13','資管系':'13','資管':'13','資訊傳播工程學系':'16','資傳系':'16', '資傳':'16','資訊工程學系':'36','資工系':'36','資工':'36','電腦與通訊工程學系':'05','電通系':'05','電通':'05', '電子工程學系':'37','電子系':'37','電工系':'37','電子':'37','電工':'37','傳播學院':'26313233','廣播電視學系':'32','廣電系':'32', '廣電':'32','新聞學系':'33','新聞系':'33','新聞':'33','廣告暨策略行銷學系':'26','廣銷系':'26','廣銷':'26','新媒體暨傳播管理學系':'31', '新傳系':'31','新傳':'31','法律學院':'4165','法律學系':'41','法律系':'41','法律':'41','財金法律系':'65','財法系':'65','財法':'65', '觀光學院':'141819','觀光事業學系':'14','觀光系':'14','觀光':'14','休閒遊憩管理學系':'18','休憩系':'18','休憩':'18', '餐旅管理學系':'19','餐旅系':'19','餐旅':'19','教育暨應用語文學院':'4243444583','應用中國文學系':'43','應中系':'43','應中':'43', '應用英語學系':'42','應英系':'42','應英':'42','應用日語學系':'44','應日系':'44','應日':'44','教育研究所':'83','華語文教學學系':'45', '華教系':'45','華教':'45','設計學院':'040921232450','商業設計學系':'21','商設系':'21','商設':'21','商品設計學系':'23','品設系':'23', '品設':'23','數位媒體設計學系':'09','數媒系':'09','數媒':'09','都市規劃與防災學系':'04','都防系':'04','都防':'04','建築學系':'24', '建築系':'24','建築':'24','動漫文創設計學士學位學程':'50','動漫學程':'50','社會科學院':'083868','公共事務與行政管理學系':'68', '公事系':'68','公事':'68','諮商與工商心理學系':'08','心理系':'08','心理':'08','犯罪防治學系':'38','犯防系':'38','犯防':'38', '健康科技學院':'394067','生物科技學系':'39','生科系':'39','生科':'39','醫療資訊與管理學系':'40','醫管系':'40','醫管':'40', '生物醫學工程學系':'67','醫工系':'67','醫工':'67','國際學院':'252747485991','國際事務與外交學士學位學程':'59', '國際企業與貿易學士學位學程':'91','新聞與大眾傳播學士學位學程':'27','時尚與創新管理學士學位學程':'25','旅遊與觀光學士學位學程':'48', '資訊科技應用與管理學士學位學程':'47','金融科技學院':'172246','經濟與金融學系':'22','經濟系':'22','經濟':'22','應用統計與資料科學學系':'17', '統資系':'17','統資':'17','金融科技應用學士':'46','金科系':'46','金科':'46' } # people_count = 0 #老師數量 # subject_count = 0 #課程數量
+
     for sentence in text:
         # index = sentence.find(',')
         # word = sentence[15:index-1]     #抓word的字
@@ -65,7 +52,7 @@ def ner_query(text: List[NerToken]):
         if 'SUBJECT' in sentence.ner:            #科目
             # subject_count += 1
             se = pinyin(word, style=Style.BOPOMOFO)  #注音
-            se = "".join([item for innerlist in se for item in innerlist])  #刪[]
+            se = "%%".join([item for innerlist in se for item in innerlist])  #刪[]
             se = re.sub('[ˊˇˋ˙]','',se)   #拿掉音調
             if 'ㄨㄟㄏㄜ' in se:
                 se = se.replace('ㄨㄟㄏㄜ','web')
@@ -73,10 +60,10 @@ def ner_query(text: List[NerToken]):
                 se = se.replace('dyson','python')
             subject.append(se)
         elif 'TIME' in sentence.ner:             #時間
-            word1 = ''; word2 = ''
+            word1 = word2 = '';
             if word == '早上' or word == '上午':
                 word1 = '01020304'
-            if  word == '下午':
+            if word == '下午':
                 word1 = '0506070809'
             if word == '晚上':
                 word1 = '40506070'
@@ -103,10 +90,10 @@ def ner_query(text: List[NerToken]):
                 else :
                     end = word[e_index]
                 
-                
                 for i in range(begin, end+1):
                     t = t + '0' + str(i)
                 word1 = t
+
             if len(word1)>0:
                 time_lesson.append(word1)
             else:
@@ -115,24 +102,28 @@ def ner_query(text: List[NerToken]):
         elif 'PEOPLE' in sentence.ner:           #老師
             # people_count += 1
             se = pinyin(word, style=Style.BOPOMOFO)  #注音
-            se = "".join([item for innerlist in se for item in innerlist])  #刪[]
+            se = "%%".join([item for innerlist in se for item in innerlist])  #刪[]
             se = re.sub('[ˊˇˋ˙]','',se)   #拿掉音調
             people.append(se)
         elif 'DEPARTMENT' in sentence.ner:       #系
+            # if word not in Department.keys():
             if '學院' in word:
                 d = Department[word]
                 for num in range(int(len(d)/2)):
                     department.append(d[num*2:num*2+2])
-                    
                 continue
             else :
                 department.append(Department[word])
         elif 'CATEGORY' in sentence.ner:         #選別
-            if '必修' in word:
-                word = '1'
-            if '選修' in word:
-                word = '4'
-            category.append(word)
+            word_zhuyin = toZhuYin(word)
+            if toZhuYin('必修') == word_zhuyin:
+                category.append('必修')
+            if toZhuYin('選修') == word_zhuyin:
+                category.append('選修')
+            if toZhuYin('通識') == word_zhuyin:
+                category.append('通識')
+            if toZhuYin('教育') == word_zhuyin:
+                category.append('教育')
         elif 'GRADE' in sentence.ner:            #年級
             if '年級' in word:
                 word = word.replace('年級','')
@@ -162,9 +153,9 @@ def ner_query(text: List[NerToken]):
             elif se == 'ㄊㄞㄅㄟ':
                 word = '台北'
             campus.append(word)
-        elif 'CURRICULUM' in sentence.ner:       #課表
-            curriculum.append(word)
-            #查課表?
+        # elif 'CURRICULUM' in sentence.ner:       #課表
+        #     curriculum.append(word)
+        #     #查課表?
         elif 'CREDIT' in sentence.ner:           #學分
             if '學分' in word:
                 word = word.replace('學分','')
@@ -228,39 +219,64 @@ def ner_query(text: List[NerToken]):
             if se == 'ㄉㄧㄥ':
                 word = '丁'
             Class.append(word)
-                
-    res = dict(people=people, category=category, grade=grade, department=department, time_lesson=time_lesson, time_week=time_week, campus=campus, subject=subject, curriculum=curriculum, credit=credit, semester=semester, education=education, internship=internship, graduate=graduate, stuclass=Class)
 
-    sql = "SELECT * FROM `TEST` WHERE "
+    res = dict(people=people, category=category, grade=grade, department=department, time_lesson=time_lesson, time_week=time_week, campus=campus, subject=subject, credit=credit, semester=semester, education=education, internship=internship, graduate=graduate, stuclass=Class)
+    print(res)
+
+    sql = "SELECT * FROM `all_course_del` WHERE "
     cond = []
+    # 制別
+    # 科目代號
+    # 科目名稱
+    # 科目名稱注音	
+    # 班級代號
+    # 班級名稱
+    # 任課教師
+    # 任課教師注音	
+    # 老師簡稱
+    # 老師簡稱注音	
+    # 上課日期／節次
+    # 年級
+    # 教室
+    # 校區
+    # 選別
+    # 學分
+    # 類別
+    # 畢業班
+    # 學期數
+    # 說明
+    # 實習課
+
     for k, v in res.items():
         # print(k, len(v))
         if k == 'subject' and len(v) > 0:
             for search in v:
-                cond.append(f"`科目名稱注音` like '%{search}%'")
-        # if k == 'time_lesson' and len(v) > 0:
-        #     for search in v:
-        #          cond.append("`上課時間` = '%s'"%(search))
+                cond.append(f"`科目名稱注音` like '%%{search}%%'")
+        if k == 'time_lesson' and len(v) > 0:
+            for search in v:
+                l = [f"`上課日期／節次` like '%%{search[i:i+2]}%%'" for i in range(0, len(search), 2)];
+            cond.append('(' + ' or '.join(l) + ')');
         if k == 'time_week' and len(v) > 0:
             l = []
             for search in v:
-                l.append(f"left(`上課時間`, 1) = '{search}'")
+                l.append(f"left(`上課日期／節次`, 1) = '{search}'")
             cond.append('(' + ' or '.join(l) + ')')
         if k == 'people' and len(v) > 0:
             for search in v:
-                cond.append(f"`老師注音` like '{search}%'")
+                cond.append(f"`任課教師注音` like '%%{search}%%'")
         if k == 'department' and len(v) > 0:
             for search in v:
-                cond.append("`班級代號` = '%s'"%(search))
+                cond.append("left(`班級代號`, 2) = '%s'"%(search))
         if k == 'category' and len(v) > 0:
             for search in v:
                 cond.append("`選別` = '%s'"%(search))
         if k == 'grade' and len(v) > 0:
             for search in v:
                 cond.append("`年級` = '%s'"%(search))
+        # TODO: 校區注音還沒搞
         if k == 'campus' and len(v) > 0:
             for search in v:
-                cond.append("`校區注音` = '%s'"%(search))
+                cond.append("`校區` = '%s'"%(search))
         # if k == 'curriculum' and len(v) > 0: #課表
         #     sql = "SELECT * FROM `TEST` WHERE " #課表怎麼印O.O
         if k == 'credit' and len(v) > 0:
@@ -268,7 +284,7 @@ def ner_query(text: List[NerToken]):
                 cond.append("`學分` = '%s'"%(search))
         if k == 'semester' and len(v) > 0:
             for search in v:
-                cond.append("`學期` = '%s'"%(search))
+                cond.append("`學期數` = '%s'"%(search))
         if k == 'education' and len(v) > 0:
             for search in v:
                 cond.append("`制別` = '%s'"%(search))
@@ -281,10 +297,15 @@ def ner_query(text: List[NerToken]):
         if k == 'Class' and len(v) > 0:
             for search in v:
                 cond.append("`班級` = '%s'"%(search))
-    
+        # TODO: 先固定為上學期
+        # cond.append("`學期數` = '上學期'");
+
+    if len(cond) == 0:
+        sql += "0";
     if len(cond) == 1:
         sql += cond[0];
+        sql += ' and `學期數` = "上學期"';
     if len(cond) > 1: 
-        sql += ' and '.join(cond)
-
+        sql += ' and '.join(cond);
+        sql += ' and `學期數` = "上學期"';
     return sql
--- python/pyserver.py
+++ python/pyserver.py
@@ -1,12 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
 from pypinyin import Style, pinyin
 from func.helper import findTeacherSubject
 from func.ner_query import ner_query
 import json
 from ckip_transformers.ckip_transformers.nlp import CkipNerChunker
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, exc
+from os import getenv
+import pandas as pd
+from bs4 import BeautifulSoup
+from pprint import pprint
 
 model_name = "./test-ner3696"
+
+load_dotenv()
+
+url = f"mysql+pymysql://{getenv('user')}:{getenv('pass')}@{getenv('host')}:{getenv('port')}/NER"
+
+engine = create_engine(url, pool_recycle=3600)
 
 print("Initializing drivers ... NER")
 ner_driver = CkipNerChunker(model_name=model_name)
@@ -43,13 +55,13 @@ def filter():
 @app.route('/api/ner', methods=['POST'])
 def ner():
     req = request.get_json()
-    print(req);
-    if len(req['text']) == 0: return json.dumps({"result": ""});
+    if len(req['text']) == 0: 
+        return json.dumps({"result": ""});
+
     if (req['multiple']):
         text = req['text'];
     else:
         text = [req['text']];
-    
     
     ner = ner_driver(text)
     
@@ -61,9 +73,103 @@ def ner():
                 "idx": [entity.idx[0], entity.idx[1]]
             } for entity in ner_item
         ]
-        
-    return json.dumps({"result": [format2json(nerr) for nerr in ner]});
-    # return json.dumps({"result": ""});
+    query_statement = ner_query(ner[0])
+    try:
+        query_result = pd.read_sql_query(query_statement, engine).to_json(orient='split', force_ascii=False);
+        query_result = json.loads(query_result);
+        response = {
+            "status": "success",
+            "result": [format2json(nerr) for nerr in ner],
+            "tbl": query_result
+        }
+        print(response)
+        return make_response(
+            json.dumps(response),
+            200
+        )
+    except exc.SQLAlchemyError:
+        response = {
+            "status": "error",
+            "sql": query_statement,
+        }
+        return make_response(
+            json.dumps(response),
+            400
+        )
 
+@app.route('/api/query', methods=['POST'])
+def query():
+    try:
+        req = request.get_json()
+        course_list = ", ".join([ str(i) for i in req['courses']])
+        query_statement = f"SELECT * FROM `all_course_del` WHERE `No.` in ({course_list}) ;"
+        query_result = pd.read_sql_query(query_statement, engine).to_json(orient='split');
+        query_result = json.loads(query_result);
+        return make_response(
+            json.dumps({
+                "status": "success",
+                "tbl": query_result
+            }),
+            200
+        )
+    except exc.SQLAlchemyError:
+        return make_response(
+            json.dumps({
+                "status": "error",
+                "sql": query_statement
+            }),
+            400
+        )
+
+@app.route('/api/uploadcirriculum', methods=['POST'])
+def cirriculum():
+    try:
+        html_doc = request.files['File'].read();
+        soup = BeautifulSoup(html_doc, 'html.parser');
+        # css selector: body > div:nth-child(1) > table:nth-child(10)
+
+        table = soup.select_one('body table[bordercolor="#77BBFF"]').prettify();
+
+        converters = {
+            '班級代號': str,
+            '科目代號／科目名稱': str,
+            '選別': str,
+            '教室': str,
+            '學分': str,
+            '是否正課': str,
+            '星期': str,
+            '起節次': str,
+            '終節次': str
+        }
+
+        df = pd.read_html(table, header = 0, converters=converters)[0]
+        df.dtypes
+
+        df2 = df.dropna()
+        df2
+
+        newdf = df2["科目代號／科目名稱"].str.split("　", expand=True)
+        newdf2 = df2.copy()
+
+        newdf2[["科目代號", "科目名稱"]] = newdf
+
+        newdf2.dtypes
+
+        df3 = newdf2.drop("科目代號／科目名稱", axis = 1)
+
+        df3 = df3[df3.columns[[8, 9, 0, 1, 2, 3, 4, 5, 6, 7]]]
+
+        return make_response(
+            json.dumps({
+                "status": "success",
+                "tbl": json.loads(df3.to_json(orient='split'))
+            }), 
+            200
+        )
+    except:
+        return make_response(
+            json.dumps({"status": "error"}),
+            400
+        )
 if __name__ == "__main__":
     app.run(port=5000)
